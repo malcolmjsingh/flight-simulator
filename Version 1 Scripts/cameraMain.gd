db804b231832ae43647aa796598acc7dfd223778
TheFallenZotan <mikezee64161@gmail.com>
2026-01-21 11:54:59 -0500

    Update and rename free_cam.gd to cameraMain.gd
    Adds orbit camera, camera switching. Requires to be attached to camera object that is a child of a Node3D for the orbiting position.

---
+++ Version 1 Scripts/cameraMain.gd
@@ -0,0 +1,124 @@
+extends Camera3D
+
+@export var moveSpeed := 0.6
+@export var freeMouseSensitivity := 1
+@export var orbitMouseSensitivity := 0.5
+@export var orbitPivot : Node3D
+
+var mouse_delta := Vector2.ZERO
+var mouse_lock := false
+var camera_state := 0
+var orbit_distance := 1.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	mouse_lock = true
+	position = Vector3(0, 1, 0)
+
+func _input(event: InputEvent) -> void:
+	# Checking button inputs
+	if event is InputEventKey and event.pressed:
+		# Unlock mouse for pause menu
+		if event.keycode == KEY_ESCAPE:
+			if mouse_lock:
+				Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+				mouse_lock = false
+			else:
+				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+				mouse_lock = true
+		
+		# Changing camera
+		if Input.is_action_just_pressed("change_camera"):
+			camera_state += 1
+			camera_state %= 2
+			print("change camera to", camera_state)
+			
+			if camera_state == 0: # Free cam
+				print(global_rotation)
+				var t: Transform3D = global_transform
+				set_as_top_level(true)
+				global_transform = t
+			
+			elif camera_state == 1: # Orbit cam
+				set_as_top_level(false)
+				position = Vector3(0, 0.5, -3)
+				rotation = Vector3(PI, 0, PI)
+				
+			elif camera_state == 2: # Cockpit cam
+				pass
+	
+	# Mouse movement handling
+	elif event is InputEventMouseMotion and mouse_lock:
+		mouse_delta += event.relative
+
+func freeCameraUpdate(delta: float) -> void:
+	"""
+	This function handles free camera update along with movement for all axises while in free camera mode.
+	
+	Args:
+	- delta (float) : Amount of time passed between calls
+	
+	Returns:
+	- None 
+	"""
+	
+	# Camera rotation stuff
+	rotation.x -= clamp(mouse_delta.y * freeMouseSensitivity * delta, -PI/2.0, PI/2.0)
+	rotation.y -= mouse_delta.x * freeMouseSensitivity * delta
+	
+	# Camera movement stuff
+	var move_delta := Vector3.ZERO
+	var forward := Vector3(-sin(rotation.y), 0, -cos(rotation.y))
+	var right := Vector3(cos(rotation.y), 0, -sin(rotation.y))
+	
+	if Input.is_action_pressed("move_forward"):
+		move_delta += forward
+	if Input.is_action_pressed("move_backwards"):
+		move_delta -= forward
+	if Input.is_action_pressed("move_right"):
+		move_delta += right
+	if Input.is_action_pressed("move_left"):
+		move_delta -= right
+	if Input.is_action_pressed("move_up"):
+		move_delta.y += 1
+	if Input.is_action_pressed("move_down"):
+		move_delta.y -= 1
+
+	position += move_delta * moveSpeed * delta
+	move_delta = Vector3.ZERO
+	
+func orbitCameraUpdate(delta: float):
+	"""
+	This function handles the orbit camera update.
+	
+	Args:
+	- delta (float) : Amount of time passed between calls
+	
+	Returns:
+	- None 
+	"""
+	var radialDistanceX = mouse_delta.x * orbitMouseSensitivity * delta
+	var radialDistanceY = mouse_delta.y * orbitMouseSensitivity * delta
+	orbitPivot.updateRotation(radialDistanceX, radialDistanceY)
+	
+func cockpitCameraUpdate(delta: float):
+	"""
+	This function is under development. Please do not use it.
+	"""
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	# Camera info for free cam
+	if camera_state == 0:
+		freeCameraUpdate(delta)
+	# Camera info for orbit cam
+	if camera_state == 1:
+		orbitCameraUpdate(delta)
+		
+	if camera_state == 2:
+		cockpitCameraUpdate(delta)
+	
+	mouse_delta = Vector2.ZERO
