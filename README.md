94eccff3760c5ece14f45f3f739bb7a7c88f366e
Malcolm <malcolmjsingh@gmail.com>
2026-03-12 08:39:32 -0400

    Project structure organized and solidified.
    In this commit the project files between different team members were merged and a specific file system structure was decided between team members.

--- README.md
+++
@@ -1,3 +0,0 @@
-# flight-simulator
-
-This is a sentance about an important topic.
---
+++ scenes/menu_scenes/main_menu/scripts/main_menu.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/ground.tscn")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+
+func _on_to_diagram_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/menu_scenes/diagram_test/diagram_test.tscn")
