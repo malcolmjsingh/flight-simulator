d5f932149c84c64df7a1e8907c79785932c8b82a
TheFallenZotan <mikezee64161@gmail.com>
2026-04-15 13:20:44 -0400

    Again

--- scenes/menu_scenes/pause_menu/scripts/pause_menu.gd
+++ scenes/menu_scenes/pause_menu/scripts/pause_menu.gd
@@ -38,4 +38,5 @@ func _on_exit_to_desktop_pressed() -> void:
 	get_tree().quit()
 
 func _on_exit_to_main_menu_pressed() -> void:
+	toggle_pause_menu()
 	get_tree().change_scene_to_file("res://scenes/menu_scenes/main_menu/main_menu.tscn")
